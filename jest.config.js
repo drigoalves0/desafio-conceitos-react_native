ce2eaa3be0289198f2dab23127eb8c9987c9dee6
drigoalves0 <drigoalves0@hotmail.com>
2020-05-16 16:11:21 -0300

    rodrigo

--- jest.config.js
+++ jest.config.js
@@ -91,7 +91,7 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: '@testing-library/react-native',
+  preset: "@testing-library/react-native",
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -126,9 +126,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [
-    "@testing-library/jest-native/extend-expect"
-  ],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
